b124a374e0fc64b3b5d91909497b72cad22e9b51
gabrielmenezes30 <menezesg792@gmailcom>
2022-01-21 10:30:21 -0300

    first commit

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,187 @@
+<template>
+    <div>
+        <Message :msg="msg"  v-show="msg"/>
+        <form id="burguer-form" @submit="createBurger">
+            <div class="input-container">
+                <label for="nome">Nome do cliente</label>
+                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite deu nome">
+            </div>
+            <div class="input-container">
+                <label for="pao">Escolha o pão</label>
+                <select name="pao" id="pao" v-model="pao">
+                    <option value="">Selecione o seu pão</option>
+                    <option  v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                        {{ pao.tipo }}
+                    </option>
+                </select>
+            </div>
+            <div class="input-container">
+                <label for="carne">Escolha a carne</label>
+                <select name="carne" id="carne" v-model="carne">
+                    <option value="">Selecione o tipo de carne</option>
+                    <option value="mamina">Mamina</option>
+                     <option  v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                        {{ carne.tipo }}
+                    </option>
+                </select>
+            </div>
+            <div id="opcionais-container" class="input-container">
+                <label id="opcionais-title" for="opcionais">Selecione os opcionais</label>
+                <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                    <span>{{ opcional.tipo }}</span>
+                </div>
+            </div>
+            <div class="input-container">
+               <input type="submit" class="submit-btn" value="Criar meu Burguer!">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import Message from './Message.vue'
+
+
+export default {
+    name: "Burger Form",
+    components:{
+        Message
+    },
+    data(){
+        return{
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            msg: null
+        }
+    },
+    methods:{
+        async getIngredientes(){
+
+            const req = await fetch("http://localhost:3000/ingredientes")
+            const data = await req.json();
+
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisdata = data.opcionais;
+
+
+        },
+        async createBurger(e){
+
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers",{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            //msg
+
+            this.msg = `Pedido realizado com sucesso. Nº do pedido: ${res.id}`
+
+
+            setTimeOut(() => this.msg ="", 3000)
+
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
+
+        }
+    },
+    mounted(){
+        this.getIngredientes();
+    }
+}
+</script>
+
+
+<style scoped>
+    #burguer-form{
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #fcba03;
+    }
+
+    input, select{
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container{
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title{
+        width: 100%;
+    }
+
+    .checkbox-container{
+        display:flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .cehckbox-container span,
+    .checkbox-container input
+    {
+        width: auto;
+    }
+
+    .checkbox-container span{
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn{
+        background: #222;
+        color: #fcab03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: .5s;
+}
+
+    .submit-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte seu Hamburguer</h1>
+    <BurguerForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from '../components/Banner.vue'
+import BurguerForm from '../components/BurgerForm.vue'
+
+export default {
+
+  name: 'Home',
+  components: {
+    Banner,
+    BurguerForm
+  }
+}
+</script>
